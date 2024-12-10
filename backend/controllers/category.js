4c62ccb8baa6c81f9cd80e0695f5607ce6eb783a
OsinnowoDavid <davidadewale151@gmail.com>
2024-12-09 23:58:30 -0800

    correct

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -96,6 +96,71 @@ const getcategories = async (req, res) => {
     }
   };
   
+  
+const update = async (req,res) =>{
 
-  export {getcategories, insertcategory}
+    
+  try{
+
+      const{name,description,price,category,subcategory,sizes,bestseller}= req.body
+
+      const image1 = req.files.image1 && req.files.image1[0]
+
+
+      // const image2 = req.files.image2 && req.files.image2[0]
+      // const image3 = req.files.image3 && req.files.image3[0]
+      // const image4 = req.files.image4 && req.files.image4[0]
+
+      const images = [image1] .filter((item) => item !== undefined)
+       let imageurl = await Promise.all(
+          images.map(async (item) =>{
+              let result = await cloudinary.uploader.upload(item.path ,{resource_type:"image"})
+
+              return result.secure_url
+              
+          })
+       )
+
+     
+       
+       // Prepare data for insertion
+  // const productData = [
+  //   name,
+  //   description,
+  //   // category,
+  //   price,
+  //   // image: imageurl,
+  //   // imageurl,
+  //   // subcategory,
+  //   // bestseller === "true" ? true : false,
+  //   // sizes,
+  //   JSON.stringify(imageurl), // Store image URLs as JSON string
+  //   date: new Date() // Current date
+  // ]
+  const productData = [name, description, price, imageurl, new Date()];
+  console.log(imageurl)
+
+      const db = await connectoDatabase()
+const sql = "update  categories set `name`= ? ,`description` = ?, `price` =? ,`image`=? ,`data`=? ,where id = ?"
+// const sql = "INSERT INTO categories (`name`, `description`,`price`,`image`,`date`) VALUES (?)";
+
+const id = req.params.id;
+await db.query(sql, [...productData, id], (err, result) => {
+if (err) {
+  console.error(err.message);
+  return res.status(500).json({ success: false, message: "Database insertion error" });
+}
+res.json({ success: true, message: "Product added successfully!" });
+});
+
+console.log("Product details:", name, description, price, category, subcategory, sizes, bestseller);
+console.log("Image URLs:", imageurl);
+
+} catch (error) {
+res.status(500).json({ success: false, message: error.message });
+console.error(error);
+}
+};
+
+  export {getcategories, insertcategory ,update}
 
